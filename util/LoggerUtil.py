1b5079969df569ecb833cb0ae9e4f24e85691edb
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2024-10-20 01:56:09 +0300

    logging fix

--- util/LoggerUtil.py
+++ util/LoggerUtil.py
@@ -7,7 +7,7 @@ class LoggerUtil:
     def get_logger(name: str) -> logging.Logger:
         """Get logger with the given name"""
         logging.basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
         )
         logger = logging.getLogger(name)
         return logger
